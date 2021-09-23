8c6ec664540c21fd85a902a7f39a2db15138ed48
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2021-09-23 23:53:24 +0530

    First additions

---
+++ Activity/Submission Form Style/main.js
@@ -0,0 +1,18 @@
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        form.reset()
+      }).catch(error => {
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
