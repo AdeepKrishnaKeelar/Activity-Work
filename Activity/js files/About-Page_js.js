c31320027a784fc398b3ef8f0af2f6db82c0cf16
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2021-10-06 21:50:18 +0530

    Quicksilver

--- Activity/js files/About-Page_js.js
+++
@@ -1,7 +0,0 @@
-function toggle() {
-    var trailer=document.querySelector(".trailer")
-    var video=document.querySelector("video")
-    trailer.classList.toggle("active");
-    video.pause();
-    video.currentTime =0;
-}
